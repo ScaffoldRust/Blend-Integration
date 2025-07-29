2158f246a84e65870c44c8c89d7e31db13f91ca9
Sandijigs <idjigheresandra@gmail.com>
2025-07-29 21:10:00 +0100

    Update dashboard page

--- apps/webapp/app/dashboard/page.tsx
+++ apps/webapp/app/dashboard/page.tsx
@@ -1,11 +1,7 @@
-import { DashboardStatsCards } from "../../components/dashboard/dashboard-stats-cards";
+// import { DashboardStatsCards } from "../../components/dashboard/dashboard-stats-cards";
 
-export default function Dashboard() {
-  return (
-    <div className="min-h-screen bg-gray-50">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-        <DashboardStatsCards />
-      </div>
-    </div>
-  );
+function page() {
+  return <div>page</div>;
 }
+
+export default page;
